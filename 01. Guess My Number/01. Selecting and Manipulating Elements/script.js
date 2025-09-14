e04a892240a845feb7c92100950f043f1fa531b6
ephraim <ephraims.818@gmail.com>
2025-09-15 03:38:43 +0600

    JS-REVICE--COMPLETE-JS---DOM--#01-GUESS-MY-NUMBER - 01. Selecting and Manipulating Elements

---
+++ 01. Guess My Number/01. Selecting and Manipulating Elements/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+document.querySelector('.message').textContent = 'Correct number! 🥳';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 15;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
